3c86446f3355a40258d6917e839b36b458aa598f
deb-panda <debadutta.panda111@gmail.com>
2025-07-31 10:41:52 +0000

    customization as per codespaces

--- python_program.py
+++ python_program.py
@@ -21,18 +21,12 @@ CONFIG = {
         "stoploss_pct": -10,
         "timeout_days": 180,
     },
-    #"csv_file": '/content/drive/MyDrive/backtest_data/deb_basket.csv',
-    #"csv_file": '/content/drive/MyDrive/backtest_data/ban_strategy_backtest_jan_2024.csv',
-    #"csv_file": '/content/drive/MyDrive/backtest_data/ban_strategy_backtest_jan_2024_v2.csv',
-    "csv_file": '/content/drive/MyDrive/backtest_data/ban_stock_v3.csv',
-    "output_folder": '/content/drive/My Drive/output'
+    "csv_file": '/workspaces/ghostwhowalks/test_data.csv',
+    "output_folder": '/workspaces/ghostwhowalks/output'
 }
 
 
 def read_csv_data(csv_file):
-    from google.colab import drive
-    drive.mount('/content/drive')
-
     def parse_date(x):
         try:
             return pd.to_datetime(x).normalize()
